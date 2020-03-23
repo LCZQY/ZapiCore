dbb8ab6b406e1ed57005237925a23c1847436820
zqy <2536156085@QQ.com>
2020-03-23 14:54:04 +0800

    添加项目文件。

---
+++ ZapiCore/JsonHelper.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace ZapiCore
+{
+    /// <summary>
+    /// Json处理
+    /// </summary>
+    public class JsonHelper
+    {
+        private static JsonSerializerSettings setting;
+
+        static JsonHelper()
+        {
+            setting = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                DateFormatString = "yyyy-MM-dd HH:mm:ss",
+                DateTimeZoneHandling = DateTimeZoneHandling.Local,
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            setting.Converters.Add(new StringEnumConverter());
+        }
+
+        /// <summary>
+        /// 对象转化字符串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToJson(object obj)
+        {
+            if (obj == null)
+            {
+                return "";
+            }
+            return JsonConvert.SerializeObject(obj, setting);
+        }
+        
+        /// <summary>
+        /// 字符串转化成对象
+        /// </summary>
+        /// <param name="json"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object ToObject(string json, Type type)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject(json, type, setting);
+        }
+
+    }
+}
