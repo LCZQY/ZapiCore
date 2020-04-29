760bd56c1efa0bd6b34e0ef60f35484479098081
zqy <2536156085@QQ.com>
2020-04-29 15:04:37 +0800

    新增 Userinfo 实体信息

---
+++ ZapiCore/Filters/SwaggerIgnoreAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZapiCore.Filters
+{
+    /// <summary>
+    /// Swagger中生成文档时忽略指定字段
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SwaggerIgnoreAttribute : Attribute { }
+}
